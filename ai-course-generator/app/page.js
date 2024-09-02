5af1053358e5c911b1f215e59494ec1a87a0f18d
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-09-02 13:47:22 +0530

    initial changes

---
+++ ai-course-generator/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    Home Page
+    <Button variant="default">Default</Button>
+   </div>
+  );
+}
