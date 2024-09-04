e6538548a0ae6ac5863249fe954dab2bdff6c58d
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-09-04 14:31:54 +0530

    change the font

--- ai-course-generator/app/dashboard/_components/AddCourse.jsx
+++ ai-course-generator/app/dashboard/_components/AddCourse.jsx
@@ -1,15 +1,19 @@
 "use client";
 import React from 'react'
+import {Button} from '../../../components/ui/button'
 import { useUser } from '@clerk/nextjs'
+
 
 function AddCourse() {
     const {user} = useUser();
   return (
  
-    <div>
+    <div class='flex items-center justify-between'>
         <div>
-            <h2 className='text-2xl'>Hello, <span className='font-bold'>{user?.fullName}</span></h2>
+            <h2 className='text-3xl'>Hello, <span className='font-bold'>{user?.fullName}</span></h2>
+            <p className='text-sm text-gray-500'>Create new course with AI, Share with friends and earn from it </p>
         </div>
+       <Button>+ Create AI Course</Button>
     </div>
   )
 }
